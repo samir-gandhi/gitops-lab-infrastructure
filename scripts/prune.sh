53044d17a315448ba4bf54cf585052328841b988
samirgandhi19 <17574913+samir-gandhi@users.noreply.github.com>
2024-11-07 14:16:04 -0700

    re-enable

--- scripts/prune.sh
+++ scripts/prune.sh
@@ -3,18 +3,16 @@
 ## THIS IS TO CLEAN THE RELEASE RELATED TO YOUR CURRENT BRANCH IN GIT.
 ## *****IT CAN BE VERY DESTRUCTIVE*****
 ## Be sure you're on the right git branch. 
-set -x
 CWD=$(dirname "$0")
 . "${CWD}/vars.sh"
 . "${CWD}/functions.sh"
 getLocalSecrets
 getEnv
 
-echo "${YELLOW}INFO: Pruning environment - ${ENV} in ${K8S_NAMESPACE} ${NC}"
-# helm uninstall "${ENV}" -n "${K8S_NAMESPACE}"
-# if test "${1}" = "--heavy" ; then
-#   kubectl delete pvc --selector=app.kubernetes.io/instance="${ENV}"
-#   kubectl delete ns "${K8S_NAMESPACE}"
-# fi
 
-echo "${GREEN} INFO: Environment pruned - ${ENV} ${NC}"
+echo "${YELLOW}INFO: Pruning environment - ${ENV} in ${K8S_NAMESPACE} ${NC}"
+helm uninstall "${ENV}" -n "${K8S_NAMESPACE}"
+if test "${1}" = "--heavy" ; then
+  kubectl delete pvc --selector=app.kubernetes.io/instance="${ENV}"
+  kubectl delete ns "${K8S_NAMESPACE}"
+fi
